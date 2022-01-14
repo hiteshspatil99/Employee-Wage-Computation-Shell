ed23b2cafdff4df2a2bc0f4bd73b9354161aafa3
Hitesh <hiteshspatil99@gmail.com>
2022-01-14 22:58:02 +0530

    [Hitesh] add : Program For Calculating  Wages Till Total Working Hours and Days Reached

---
+++ EmployeeWage.sh
@@ -0,0 +1,42 @@
+#Calculate Wages till a condition of total working hours or Days is reached for a Month
+
+#!/bin/bash -x
+wage_per_hr=20
+max_hrs=100
+max_days=20
+total_working_hrs=0
+total_working_days=0
+
+emp_attendance=$(( RANDOM % 2 ))
+case $emp_attendance in
+0)
+ echo "Employee is Present"
+    while(( total_working_hrs < max_hrs && total_working_days < max_days ))
+     do
+      (( total_working_days++ ))
+      attendance=$(( RANDOM % 2 ))
+      case $attendance in
+        0)
+            echo "Employee is Full Time present"
+            no_of_hrs_per_day=8
+            (( total_working_hrs += no_of_hrs_per_day ))
+            total_salary=$(( total_working_hrs * wage_per_hr ))
+            echo "Total salary is $total_salary"
+            ;;
+        1)
+            echo "Employee is Part Time present"
+            no_of_hrs_per_day=4
+            (( total_working_hrs += no_of_hrs_per_day ))
+            total_salary=$(( total_working_hrs * wage_per_hr ))
+            echo "Total salary is $total_salary"
+            ;;
+        esac
+     done
+;;
+1)
+    echo "Employee is Absent"
+    no_of_hrs_per_day=0
+    ;;
+    esac
+
+
