8102cf5fef004f82739d00af31b9e3e0dd0c0e98
Hitesh Patil <92395276+hiteshspatil99@users.noreply.github.com>
2022-01-22 09:44:25 +0530

    Update EmployeeWage.sh

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,4 +1,10 @@
-#Check Employee is Present or Absent
+'''
+@Author: Hitesh Patil
+@Date: 09-01-2022 11:05:46
+@Last Modified by: Hitesh Patil
+@Last Modified time: 09-01-2022 23:40:00
+@Title : Check Employee is Present or Absent
+'''
 
 #!/bin/bash
 attendence=$(( RANDOM % 2 ))
